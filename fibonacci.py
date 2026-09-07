f60359715e4dc1a33739d5750f476750b9f07e83
2RoN4eG <2RoN4eG@mail.ru>
2026-09-07 15:13:16 +0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,5 @@
 def fibonacci(number):
-    """A generator for the Fibonacci sequence."""
+    """Generator for the Fibonacci sequence."""
     lhs, rhs = 0, 1
     for _ in range(number):
         yield lhs
