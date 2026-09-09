9a665b641fc6b7bf9c66456062bb02ae3f7fa88a
Mikalai Bohdan <2RoN4eG@gmail.com>
2026-09-09 18:25:08 +0300

    modified order_visitor.py

--- order_visitor.py
+++ order_visitor.py
@@ -1,17 +1,19 @@
 class OrderVisitor:
+    """Order visitor class"""
+
     def visit(self, order):
         method_name = f'visit_{order["status"]}'
         method = getattr(self, method_name, self.visit_unknown)
         return method(order)
-    
+
     def visit_pending(self, order):
-        return "Заказ ожидает обработки"
-    
+        return f'Заказ {order["id"]} ожидает обработки'
+
     def visit_shipped(self, order):
-        return "Заказ в пути"
-    
+        return f'Заказ {order["id"]} в пути'
+
     def visit_delivered(self, order):
-        return "Заказ доставлен"
-    
+        return f'Заказ {order["id"]} доставлен'
+
     def visit_unknown(self, order):
-        return "Неизвестный статус"
+        return 'Неизвестный статус'
