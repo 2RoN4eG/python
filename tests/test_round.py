f316eefc443e284f89fea81c23d9c2e795fb60d2
Mikalai Bohdan <2RoN4eG@gmail.com>
2026-09-11 21:59:26 +0300

    modified test_round.py

--- tests/test_round.py
+++ tests/test_round.py
@@ -1,12 +1,17 @@
 import pytest
 
 @pytest.mark.parametrize("value,expected", [
+    (0.4, 0),
+    (0.5, 0),
+    (0.6, 1),
     (1.4, 1),
     (1.5, 2),
     (1.6, 2),
     (2.4, 2),
     (2.5, 2),
-    (2.6, 3)
+    (2.6, 3),
+    (3.4, 3),
+    (3.5, 4)
 ])
 def test_round(value, expected):
     assert round(value) == expected
@@ -20,7 +25,8 @@ def test_round(value, expected):
     (1.6, 2),
     (2.4, 2),
     (2.5, 3),
-    (2.6, 3)
+    (2.6, 3),
+    (3.4, 3)
 ])
 def test_round_(value, expected):
     from round import round_
