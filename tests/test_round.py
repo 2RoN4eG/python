db2a00a5e2b8492b2691a22ff6104fce6f065032
Mikalai Bohdan <2RoN4eG@gmail.com>
2026-09-11 21:33:39 +0300

    added test_round.py

---
+++ tests/test_round.py
@@ -0,0 +1,28 @@
+import pytest
+
+@pytest.mark.parametrize("value,expected", [
+    (1.4, 1),
+    (1.5, 2),
+    (1.6, 2),
+    (2.4, 2),
+    (2.5, 2),
+    (2.6, 3)
+])
+def test_round(value, expected):
+    assert round(value) == expected
+
+@pytest.mark.parametrize("value,expected", [
+    (0.4, 0),
+    (0.5, 1),
+    (0.6, 1),
+    (1.4, 1),
+    (1.5, 2),
+    (1.6, 2),
+    (2.4, 2),
+    (2.5, 3),
+    (2.6, 3)
+])
+def test_round_(value, expected):
+    from round import round_
+
+    assert round_(value) == expected
